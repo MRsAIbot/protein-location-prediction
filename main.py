ae8cdb3de286d80d723a62be95f251278399847a
Tobias <tobias@tobiasrijken.com>
2015-02-22 18:49:40 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from Bio import SeqIO
+import numpy as np
+
+files = ['cyto.fasta', 'mito.fasta', 'nucleus.fasta', 'secreted.fasta']
+
+def read_data():
+	pass
+
+def construct_trainset():
+	pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
